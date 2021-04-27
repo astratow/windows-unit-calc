0ebc27b0af8dbcc6724ed2291f024709a7a6540d
astratow <davno_pora@mail.com>
2021-04-27 20:50:17 +0100

    window unit calculator app.js changed

---
+++ app.js
@@ -0,0 +1,16 @@
+function oneHorizontalBarSize(width){
+  return width - 24; 
+}
+function oneHorizontalBarEqualDistance(height, barSize){
+    return ((height - barSize)/2)
+}
+const unit = {
+    width: 0;
+    height: 0;
+    barSize: 18;
+    //barToSpacerBottom: 0;
+    //barToSpacerTop: 0;
+    //barToSpacerRight: 0;
+    //barToSpacerLeft: 0;
+    
+}
