ed1e584bbc616fb786e4c8d8a8aafcfebfde316f
astratow <davno_pora@mail.com>
2021-07-05 06:05:13 +0100

    start fresh

--- app.js
+++ app.js
@@ -1,65 +1,70 @@
 /*app.js*/
-function mult(width, height) {
-	var horizontalBar = width - 27;
-//	x = 2 * value;
-//	y = 3 * value;
+
+barSize = document.querySelector("#bar-size option:checked").value;
+//barSize= barSizeSelect.options[barSizeSelect.selectedIndex].value;
+console.log(barSize);
+/*function mult(value) {
+	var horizontalBar = value - 27;
+	x = 2 * value;
+	y = 3 * value;
 
 	document.getElementById('results').value = horizontalBar;
-//	document.getElementById('out3x').value = y;
+	document.getElementById('out3x').value = y;
 }
-//const elem = id => document.getElementById(id);
-//const btn = document.getElementById('btn');
-//var width =  document.getElementById("width").value;
-//var height = document.getElementById("height").value;
+const elem = id => document.getElementById(id);
+const btn = document.getElementById('btn');
+var width =  document.getElementById("width").value;
+var height = document.getElementById("height").value;
 var barSize = 18;
-//document.getElementById('results').innerHTML = oneHorizontalBarEqualDistance;
-/*btn.addEventListener("click", function(){
+document.getElementById('results').innerHTML = oneHorizontalBarEqualDistance;
+btn.addEventListener("click", function(){
                        document.getElementById('results').innerHTML = oneHorizontalBarEqualDistance;
-                       });*/
+                       });
 
-//console.log(width);
-//console.log(height);
+console.log(width);
+console.log(height);
 function showResults() {
-	var width =  document.getElementById("width").value;
-	var height = document.getElementById("height").value;
-	width = parseFloat(width);
-    height = parseFloat(height);
-    var result = document.getElementById('results').value;
+	var width =  Number(document.getElementById("width").value);
+	var height = Number(document.getElementById("height").value);
+	
+	var result = document.getElementById('results').value;
 	result = oneHorizontalBarEqualDistance(width);
 	console.log(result);
-//	result.innerHTML = oneHorizontalBarEqualDistance();
-//    c = document.getElementById('n1').value;
-//    console.log(c);
-//    console.log('distance');
-//    console.log(typeof result);
-//    console.log(result);    
-//    console.log(typeof height);
+	result.innerHTML = oneHorizontalBarEqualDistance();
+    c = document.getElementById('n1').value;
+    console.log(c);
+    console.log('distance');
+    console.log(typeof result);
+    console.log(result);    
+    console.log(typeof height);
 }
 
 function oneHorizontalBarSize(width){
+	var width =  Number(document.getElementById("width").value);
   return width - 24; 
 }
 function oneHorizontalBarEqualDistance(height, barSize){
     return ((height - barSize)/2);
 }
 
-//const unit = {
-//    width: 0;
-//    height: 0;
-//    barSize: 18;
-//    //barToSpacerBottom: 0;
-//    //barToSpacerTop: 0;
-//    //barToSpacerRight: 0;
-//    //barToSpacerLeft: 0;
-//    
-//}
+const unit = {
+    width: 0;
+    height: 0;
+    barSize: 18;
+    barToSpacerBottom: 0;
+    barToSpacerTop: 0;
+    barToSpacerRight: 0;
+    barToSpacerLeft: 0;
+    
+}
 
 
 function calcFrame(){
     p.innerHTML = width - 27;
 }
-//function getInputValue(){
-//    width = getElementsByClassName(width);
-//    height = getElementsByClassName(height);
-//}
-//getInputValue()
+function getInputValue(){
+    width = getElementsByClassName(width);
+    height = getElementsByClassName(height);
+}
+getInputValue()
+*/
